6ba9f55c9618d2defca737118abf8b9a2ca60682
Timothy Ting <timothy@tune.com>
2017-10-13 16:07:19 -0700

    Change data location

--- data_cleaner_discretizer.py
+++ data_cleaner_discretizer.py
@@ -1,6 +1,6 @@
 # This Python 3 environment comes with many helpful analytics libraries installed
 # It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
+# For example, here's several helpful packages to load in
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
@@ -8,24 +8,27 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 # Input data files are available in the "../input/" directory.
 # For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
-from subprocess import check_output
-print(check_output(["ls", "../input"]).decode("utf8"))
+# from subprocess import check_output
+# print(check_output(["ls", "../input"]).decode("utf8"))
 
 # Any results you write to the current directory are saved as output.
-input = pd.read_csv("../input/train.csv")
+input = pd.read_csv("train.csv")
 print("SIZE: %d %d" % input.shape)
 
-titles = {}
+input.drop(["PassengerId"], axis=1)
+input["New Gender"] = input["Gender"].apply()
 
-for index,row in input.iterrows():
-    name = row["Name"]
-    last_name, rest_of_name = name.split(", ", 1)
-    title, first_names = rest_of_name.split(' ', 1)
-    if title not in titles:
-        titles[title] = 1
-    else:
-        titles[ title ] += 1
-    
-for title, title_count in titles.items():
-    print("%s: %d" % (title, title_count))
+# titles = {}
+
+# for index,row in input.iterrows():
+#     name = row["Name"]
+#     last_name, rest_of_name = name.split(", ", 1)
+#     title, first_names = rest_of_name.split(' ', 1)
+#     if title not in titles:
+#         titles[title] = 1
+#     else:
+#         titles[ title ] += 1
+
+# for title, title_count in titles.items():
+#     print("%s: %d" % (title, title_count))
 
